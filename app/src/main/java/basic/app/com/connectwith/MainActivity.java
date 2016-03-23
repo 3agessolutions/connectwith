98a01de97315ed2a1940da5efd827de6f38926ad
Nagaraju <nagarajueac12@gmail.com>
2016-03-23 22:18:18 +0530

    Original Code for commit

--- app/src/main/java/basic/app/com/connectwith/MainActivity.java
+++ app/src/main/java/basic/app/com/connectwith/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -42,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        Log.d("test", "welcome");
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
